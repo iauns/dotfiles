dbf23351e3f7d4301bcea0481cbcdc5ee0b87436
James Hughes <jhughes@sci.utah.edu>
2013-07-04 15:22:50 -0600

    Update directories of spire debug window.

--- .tmuxifier_layouts/SpireDebug.window.sh
+++ .tmuxifier_layouts/SpireDebug.window.sh
@@ -1,9 +1,10 @@
 # Spire window.
-window_root "~/sci/spire"
+window_root "~/prosp/spire"
 
 new_window "spire-debug"
 
 split_h 50
+run_cmd "cd ~/sci/spire"
 split_v 20
 run_cmd "echo 'Spire log' > /tmp/SpireLog; and tail -f /tmp/SpireLog"
 
