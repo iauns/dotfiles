2ce91e712daaab2997fd5676074261f2ee1d5bb9
James Hughes <jhughes@sci.utah.edu>
2013-10-19 10:11:11 -0600

    gnu agent automatic startup.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,6 +38,9 @@ set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
 
+# Load external scripts.
+. ~/.config/fish/gnupg.fish
+
 function man
   vim -c "Man $argv" -c "only"
 end
